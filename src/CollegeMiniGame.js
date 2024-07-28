08f30107c44e5a33271bd1fd00fe3dd32375ab17
Micah C <micahchen004@gmail.com>
2024-07-28 17:55:07 +0800

    fixed spacing

--- src/CollegeMiniGame.js
+++ src/CollegeMiniGame.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Confetti from 'react-confetti';
 import './CollegeMiniGame.css';
 
-const CollegeMiniGame = ({ colleges }) => {
+const CollegeMiniGame = ({ pathName, colleges }) => {
   const [revealed, setRevealed] = useState({});
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
 
@@ -19,14 +19,18 @@ const CollegeMiniGame = ({ colleges }) => {
       <div className="college-mini-game">
         {colleges.map((college) => (
           <div key={college.name} className="college-envelope" onClick={() => handleReveal(college.name)}>
-            <p>{college.name}</p>
+            <p>{college.name}</p> {}
+            <img src={`${process.env.PUBLIC_URL}/envelope.svg`} alt="Envelope" className={`envelope-icon ${revealed[college.name] ? 'opened' : ''}`} />
             {revealed[college.name] && (
-              <>
-                <p>{college.status === 'accepted' ? 'Accepted!' : 'Rejected'}</p>
+              <div className="letter">
+                <p>Dear Student,</p> {}
+                <p>{college.status === 'accepted' ? 'Congratulations! You have been accepted!' : 'We regret to inform you that you have been rejected.'}</p>
                 {college.status === 'accepted' && (
                   <Confetti width={dimensions.width} height={dimensions.height} />
                 )}
-              </>
+                <p>Regards,</p>
+                <p>Headmaster of School</p>
+              </div>
             )}
           </div>
         ))}
